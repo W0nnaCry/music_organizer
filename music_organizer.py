f81894c791e07d49b928550847ad3e5e9847e52f
Lucky Man <kirillblaze2@gmail.com>
2024-09-22 15:31:10 +0300

    refactoring

--- music_organizer.py
+++ music_organizer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from shutil import move 
+
 import os
 import shutil
 import argparse
+
 
 
 def get_performer(track, delimiter):
@@ -11,6 +12,7 @@ def get_performer(track, delimiter):
         return track.split(delimiter)[0].strip()
     else:
         return None
+
 
 def split_by_performers(tracks, delimiter, unrecognized_tracks_directory_name, is_case_sensitive):
     splitted_tracks = {unrecognized_tracks_directory_name : []}
@@ -34,6 +36,7 @@ def split_by_performers(tracks, delimiter, unrecognized_tracks_directory_name, i
             splitted_tracks[unrecognized_tracks_directory_name].append(track)
     return splitted_tracks
 
+
 def get_tracks(directory):
     entries = os.listdir(directory)
     tracks = []
@@ -41,6 +44,7 @@ def get_tracks(directory):
         if os.path.isfile(os.path.join(directory, entry)):
             tracks.append(entry)
     return tracks
+
 
 def organize_tracks(track_groups, tracks_directory, output_directory, is_copy_mode, is_verbose):
     for group_name, group_tracks in track_groups.items():
@@ -60,6 +64,7 @@ def organize_tracks(track_groups, tracks_directory, output_directory, is_copy_mo
                     print('move track \"' + track_path + '\" to \"' + group_directory + '\"')
                 shutil.move(track_path, group_directory)
 
+
 def review_track_groups(track_groups):
     for group_name, group_tracks in track_groups.items():
         print(group_name)
@@ -67,56 +72,59 @@ def review_track_groups(track_groups):
             print('\t', track)
         print('\n')
 
-def main():
 
-    args_parser = argparse.ArgumentParser(
-        prog='music_organizer', 
-        description='Organize your music library',
-        epilog='Have a fun'
-    )
-    default_music_directory = '.'
-    args_parser.add_argument('-i', '--input', default=default_music_directory)
-    args_parser.add_argument('-o', '--output', default=default_music_directory)
-    args_parser.add_argument(
-        '-c', '--copy', 
-        action='store_true', 
-        default=False, 
-        help='copying tracks instead of moving'
-    )
-    args_parser.add_argument(
-        '-s', '--casesens', 
-        action='store_true', 
-        default=False,
-        help='enable case-sensitive performer determination'
-    )
-    args_parser.add_argument(
-        '-u', '--unrecdir', default='other', 
-        help='directory for tracks whose performer could not be determined'
-    )
-    args_parser.add_argument(
-        '-d', '--delimiter', 
-        default=' - ',
-        help='delimiter used to determine track performer (default: \' - \')'
-    )
-    args_parser.add_argument('-v', '--verbose', action='store_true', default=False)
-    args_parser.add_argument(
-        '-r', '--review', action='store_true', default=False,
-        help='only display produced organization tracks scheme, but do not write changes to file system'
-    )
+class MusicOrganizerArgsParser:
     
+    def __init__(self):
+        self.args_parser = argparse.ArgumentParser(
+            prog='music_organizer', 
+            description='Organize your music library',
+            epilog='Have a fun'
+        )
+        default_music_directory = '.'
+        self.args_parser.add_argument('-i', '--input', default=default_music_directory)
+        self.args_parser.add_argument('-o', '--output', default=default_music_directory)
+        self.args_parser.add_argument(
+            '-c', '--copy', 
+            action='store_true', 
+            default=False, 
+            help='copying tracks instead of moving'
+        )
+        self.args_parser.add_argument(
+            '-s', '--casesens', 
+            action='store_true', 
+            default=False,
+            help='enable case-sensitive performer determination'
+        )
+        self.args_parser.add_argument(
+            '-u', '--unrecdir', default='other', 
+            help='directory for tracks whose performer could not be determined'
+        )
+        self.args_parser.add_argument(
+            '-d', '--delimiter', 
+            default=' - ',
+            help='delimiter used to determine track performer (default: \' - \')'
+        )
+        self.args_parser.add_argument('-v', '--verbose', action='store_true', default=False)
+        self.args_parser.add_argument(
+            '-r', '--review', action='store_true', default=False,
+            help='only display produced organization tracks scheme, but do not write changes to file system'
+        )
+
+    def parse_args(self):
+        return self.args_parser.parse_args()
+
+
+def main():
+    args_parser = MusicOrganizerArgsParser()
     args = args_parser.parse_args()
     
-    input_directory = args.input
-    output_directory = args.output
-    unrecognized_tracks_directory_name = args.unrecdir
-    delimiter = args.delimiter
-    
-    all_tracks = get_tracks(input_directory)
-    track_groups = split_by_performers(all_tracks, delimiter, unrecognized_tracks_directory_name, args.casesens)
+    all_tracks = get_tracks(args.input)
+    track_groups = split_by_performers(all_tracks, args.delimiter, args.unrecdir, args.casesens)
 
     if args.review:
         review_track_groups(track_groups)
     else:
-        organize_tracks(track_groups, input_directory, output_directory, args.copy, args.verbose)
+        organize_tracks(track_groups, args.input, args.output, args.copy, args.verbose)
 
 main()
